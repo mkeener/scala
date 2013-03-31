6d37bf0f99bcf63e453e0edbaabed660d66daffa
Matthew Keener <MatthewCKeener@gmail.com>
2013-03-31 18:27:04 -0400

    Updating gitignore and first exercise in proj-recfun

--- progfun-recfun/progfun-recfun/src/main/scala/recfun/Main.scala
+++ progfun-recfun/progfun-recfun/src/main/scala/recfun/Main.scala
@@ -14,7 +14,10 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if(r == 0 || c == 0 || c == r) 1
+    else pascal(c-1, r-1) + pascal(c, r-1)
+  }
 
   /**
    * Exercise 2
